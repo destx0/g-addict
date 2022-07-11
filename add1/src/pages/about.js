00b34cdebcc3b86ccd0ff742701745c2228304d4
destx0 <destisdbest@gmail.com>
2022-07-11 23:41:08 +0530

    dsff

--- add1/src/pages/about.js
+++ add1/src/pages/about.js
@@ -1,14 +1,21 @@
 import React from "react"
 import Layout from "../components/Layout"
+import styled from "styled-components"
 
 const about = () => {
   return (
     <>
       <Layout>
-        <div>about page</div>
+        <Wrapper>
+          about
+          <h1>Wrapped</h1>
+        </Wrapper>
       </Layout>
     </>
   )
 }
 
+const Wrapper = styled.section`
+  color: blue;
+`
 export default about
