efb9b4307991b081c5bbd0f7b9300cf8b8c3d0a8
destx0 <destisdbest@gmail.com>
2022-07-13 11:17:11 +0530

    ddssa

--- add1/src/pages/about.js
+++ add1/src/pages/about.js
@@ -1,12 +1,34 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
+
 // import styled from "styled-components"
 
-const about = () => {
+const About = () => {
   return (
     <>
       <Layout>
-        <h1>about</h1>
+        <main className="page">
+          <section className="about-page">
+            <article>
+              <h2>Lorem ipsum dolor sit amet consectetur,</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Consequatur a iusto consequuntur odit atque fugiat soluta
+                aliquid at tenetur mollitia,
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Consequatur a iusto consequuntur odit atque fugiat soluta
+                aliquid at tenetur mollitia,
+              </p>
+
+              <Link className="btn">contact</Link>
+            </article>
+            <StaticImage src="../assets/images/about.jpeg" alt="image" />
+          </section>
+        </main>
       </Layout>
     </>
   )
@@ -15,4 +37,4 @@ const about = () => {
 // const Wrapper = styled.section`
 //   color: blue;
 // `
-export default about
+export default About
