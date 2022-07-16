efd24f5943109464ccc41a78e5d6b8f609d2a126
destx0 <destisdbest@gmail.com>
2022-07-16 15:55:35 +0530

    gfhfgh

--- add1/src/examples/gallery.js
+++ add1/src/examples/gallery.js
@@ -1,7 +1,59 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+import styled from "styled-components"
 
+const query = graphql`
+  {
+    allFile(filter: { extension: { ne: "svg" } }) {
+      nodes {
+        name
+        childImageSharp {
+          gatsbyImageData(
+            layout: FIXED
+            placeholder: TRACED_SVG
+            tracedSVGOptions: {}
+            width: 200
+            height: 200
+          )
+        }
+      }
+    }
+  }
+`
 const Gallery = () => {
-  return <div>Gallery</div>
+  const data = useStaticQuery(query)
+  const nodes = data.allFile.nodes
+  console.log(data)
+  return (
+    <Wrapper>
+      {nodes.map((image, index) => {
+        const { name } = image
+        return (
+          <article key={index} className="item">
+            <GatsbyImage
+              image={image.childImageSharp.gatsbyImageData}
+              alt={name}
+              className="gallery-img"
+            />
+            <p>{name}</p>
+          </article>
+        )
+      })}
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  /* padding: 2em; */
+  .item {
+    margin: 1em;
+  }
+  .gallery-img {
+    border-radius: 1em;
+  }
+`
 
 export default Gallery
